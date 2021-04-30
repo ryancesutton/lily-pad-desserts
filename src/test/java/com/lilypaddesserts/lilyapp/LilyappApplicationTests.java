52aa3184d529d60ffce2c575be3b1ab016d9715a
Ry Sutton <ryancesutton@gmail.com>
2021-04-30 15:54:33 -0500

    add application properties to gitignore

---
+++ src/test/java/com/lilypaddesserts/lilyapp/LilyappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lilypaddesserts.lilyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LilyappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
